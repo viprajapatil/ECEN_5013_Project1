ce94bafc1efd5eea2046cec3d69ed434217498e4
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-03-17 21:52:50 -0600

    Update test_temp.c

--- final/final_files/test/test_temp.c
+++ final/final_files/test/test_temp.c
@@ -47,6 +47,53 @@ void test_thigh_reg(void **state)
 	assert_int_equal(status, wpr);
 }
 
+void test_temp_c(void **state)
+{
+	int fd = temp_init();
+	float temp = read_temp_reg(fd, Celsius);
+	int status;
+	if (temp < -55 && temp > 128)
+		status = 1;
+	else status = 0;	
+	assert_int_equal(status, 0);
+}
+
+void test_temp_k(void **state)
+{
+	int fd = temp_init();
+	float tempc = read_temp_reg(fd, Celsius);
+	float tempk = read_temp_reg(fd, Kelvin);
+	int status;
+	if (tempc < -55 && tempc > 128)
+		status = 1;
+	else 
+	{
+		if(tempk == tempc+273.5)
+			status = 0;
+		else status = 1;
+	}
+	assert_int_equal(status, 0);
+}
+
+void test_temp_f(void **state)
+{
+	int fd = temp_init();
+	float tempc = read_temp_reg(fd, Celsius);
+	float tempf = read_temp_reg(fd, Fahrenheit);
+	int status;
+	if (tempc < -55 && tempc > 128)
+		status = 1;
+	else 
+	{
+		tempc = tempc * 1.8;
+		tempc = tempc + 32;
+		if(tempf == tempc)
+			status = 0;
+		else status = 1;
+	}
+	assert_int_equal(status, 0);
+}
+
 int main(int argc, char **argv)
 {
   const struct UnitTest tests[] = 
@@ -56,6 +103,9 @@ int main(int argc, char **argv)
 	cmocka_unit_test(test_config_reg),
 	cmocka_unit_test(test_tlow_reg),
 	cmocka_unit_test(test_thigh_reg),
+	cmocka_unit_test(test_temp_c),
+	cmocka_unit_test(test_temp_k),
+	 cmocka_unit_test(test_temp_f),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
