913822b16ef04a6d424f19995bb9edeb3e054c81
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-03-18 15:04:33 -0600

    Update userled.h

--- FinalFolder/userled.h
+++ FinalFolder/userled.h
@@ -1,3 +1,9 @@
+/*
+@file - userled.h
+@brief - Header file for turning user leds on beaglebone green ON/OFF 
+@author - Nikhil Divekar & Vipraja Patil
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
