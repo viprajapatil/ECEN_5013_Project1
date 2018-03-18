1d1945e39ad2ed834bc4f454791c1240e17452e5
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-03-17 23:28:16 -0600

    Update temp.h

--- final/final_files/test/temp.h
+++ final/final_files/test/temp.h
@@ -1,3 +1,9 @@
+/*
+@file - temp.h
+@brief - Header file of temp.c which includes all the functions for writing and reading various TMP102 registers
+@author - Nikhil Divekar & Vipraja Patil
+*/
+
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
@@ -39,23 +45,79 @@ enum{
 #define MAX_MSG_SIZE 256
 #define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
 
+/*
+@brief - 	write_pointer_reg
+			Write to pointer register
+@param -	file descriptor
+			Address of the register value to be written
+@return - 	value written to the register 
+*/
 uint8_t write_pointer_reg(int fd, uint8_t reg);
 
+/*
+@brief - 	write_thigh_reg
+			Write to THIGH register
+@param -	file descriptor
+			data to be written in the register
+@return - 	value written to the register 
+*/
 uint8_t read_pointer_reg(int fd);
 
+/*
+@brief - 	read_tlow_reg
+			Read value from TLOW register
+@param -	file descriptor
+@return - 	value read from the register 
+*/
 uint16_t read_tlow_reg(int fd);
 
 uint16_t write_tlow_reg(int fd, uint16_t reg);
-
+/*
+@brief - 	read_thigh_reg
+			Read value from THIGH register
+@param -	file descriptor
+@return - 	value read from the register 
+*/
 uint16_t read_thigh_reg(int fd);
-
+/*
+@brief - 	write_thigh_reg
+			Write to THIGH register
+@param -	file descriptor
+			data to be written in the register
+@return - 	value written to the register 
+*/
 uint16_t write_thigh_reg(int fd, uint16_t reg);
 
+/*
+@brief - 	read_temp_reg
+			Read temperature value from temperature register in Celsius, Kelvin or Fahrenheit
+@param -	file descriptor
+			Unit of temperature: Celsius, Kelvin or Fahrenheit
+@return - 	Temperature value
+*/
 float read_temp_reg(int fd, int unit);
 
+/*
+@brief - 	write_config_reg
+			Write to config register
+@param -	file descriptor
+			data to be written in the register
+@return - 	value written to the register 
+*/
 uint16_t write_config_reg(int fd, uint16_t reg);
-//uint16_t write_configreg(int fd, uint16_t config_val);
 
+/*
+@brief - 	read_config_reg
+			Read value from config register
+@param -	file descriptor
+@return - 	Value read from the register
+*/
 uint16_t read_config_reg(int fd);
-//uint16_t read_configreg(int fd, uint16_t *res);
+
+/*
+@brief - 	temp_init
+			Initializing TMP102 sensor 
+@param -	
+@return - 	file descriptor
+*/
 int temp_init();
