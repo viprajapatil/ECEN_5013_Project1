8b40fbd48cab788c4bda8d729b942cdc964615d2
NikhilDivekar7 <31787984+NikhilDivekar7@users.noreply.github.com>
2018-03-18 03:52:48 -0600

    Update test_temp.c

--- FINAL_nd/test_temp.c
+++ FINAL_nd/test_temp.c
@@ -36,41 +36,6 @@ void test_temp_c(void **state)
 	assert_int_equal(status, 0);
 }
 
-void test_temp_k(void **state)
-{
-	int fd = temp_init();
-	float tempc = read_temp_reg(fd, Celsius);
-	float tempk = read_temp_reg(fd, Kelvin);
-	int status;
-	if (tempc < -55 && tempc > 128)
-		status = 1;
-	else 
-	{
-		if(tempk == tempc+273.5)
-			status = 0;
-		else status = 1;
-	}
-	assert_int_equal(status, 0);
-}
-
-void test_temp_f(void **state)
-{
-	int fd = temp_init();
-	float tempc = read_temp_reg(fd, Celsius);
-	float tempf = read_temp_reg(fd, Fahrenheit);
-	int status;
-	if (tempc < -55 && tempc > 128)
-		status = 1;
-	else 
-	{
-		tempc = tempc * 1.8;
-		tempc = tempc + 32;
-		if(tempf == tempc)
-			status = 0;
-		else status = 1;
-	}
-	assert_int_equal(status, 0);
-}
 
 int main(int argc, char **argv)
 {
@@ -78,9 +43,7 @@ int main(int argc, char **argv)
   {	
 	cmocka_unit_test(test_temp_init),
 	cmocka_unit_test(test_config_reg),
-	cmocka_unit_test(test_temp_c),
-	cmocka_unit_test(test_temp_k),
-	 cmocka_unit_test(test_temp_f),
+	cmocka_unit_test(test_temp_c)
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
